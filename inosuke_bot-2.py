dcba7d2c2f3ed497bc6bf48cbbb19e29d7e65ba9
sodohu1 <sodohu1@outlook.com>
2026-09-13 20:20:36 +0530

    Update inosuke_bot-2.py

--- inosuke_bot-2.py
+++ inosuke_bot-2.py
@@ -57,7 +57,7 @@ from playwright.async_api import async_playwright
 # ═══════════════════════════════════════════════════════════════
 #  CONFIG — EDIT THESE BEFORE RUNNING
 # ═══════════════════════════════════════════════════════════════
-BOT_TOKEN        = "8601021110:AAEq3_0Jutw4t1YEKQpTUzYd8ZSpspmPcyE"   # @BotFather token
+BOT_TOKEN        = "8601021110:AAEZOGSXfQypJG_L24jtmbQxvTotH_QQ4Ts"   # @BotFather token
 ADMIN_IDS        = [8609127164]        # your Telegram user ID(s)
 REQUIRE_APPROVAL = False              # True = admin must approve each user
 
